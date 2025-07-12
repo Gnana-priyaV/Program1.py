50e2a2500d0ba6d990ae844462f6b8ce8b50e1f8
Gnana-priyaV <gnanapriya9740@gmail.com>
2025-07-12 11:59:17 +0530

    Update Program2.py

--- Program2.py
+++ Program2.py
@@ -1,1 +1,10 @@
+def generate_series(a: int):
+    series = []
+    for i in range(a):
+        series.append(2 * i + 1)
+    return series
+
+
+a = int(input("Enter value of a: "))
+print("Output:", generate_series(a))
 
